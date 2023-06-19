5f74f5e4669664dd29e1f41f437596d6e9b74a78
Xiphereal <lozanojalex@gmail.com>
2023-06-19 22:35:54 +0200

    feat: add "prof of concept" for allowing the Input to display the interaction with the Bed

--- Code/Control/Player.cs
+++ Code/Control/Player.cs
@@ -1,8 +1,14 @@
-﻿namespace Control
+﻿using Domain.Actions;
+using Domain.Furniture;
+using System.Collections.Generic;
+
+namespace Control
 {
     public class Player
     {
-        public void InteractWith(Domain.Furniture.Bed bed)
-        { }
+        public IEnumerable<IAction> InteractWith(Bed bed)
+        {
+            return bed.AvailableActions();
+        }
     }
 }
--- Code/Inputs/Bed.cs
+++ Code/Inputs/Bed.cs
@@ -1,10 +1,35 @@
+using Control;
+
 namespace Godot
 {
-    public partial class Bed : Node
+    public partial class Bed : Node3D
     {
+        private Player player;
+        private bool isMouseOver = false;
+
+        public override void _Ready()
+        {
+            player = new Player();
+        }
+
         public override void _Input(InputEvent @event)
         {
-            GD.Print(@event.AsText());
+            if (@event is InputEventMouseButton mouseButton
+                && mouseButton.Pressed
+                && isMouseOver)
+            {
+                GD.Print(string.Join(", ", player.InteractWith(new Domain.Furniture.Bed())));
+            }
+        }
+
+        public void OnMatressMouseEntered()
+        {
+            isMouseOver = true;
+        }
+
+        public void OnMatressMouseExited()
+        {
+            isMouseOver = false;
         }
     }
 }
