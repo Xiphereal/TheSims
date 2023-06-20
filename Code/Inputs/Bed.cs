1719f823b65956966043cb24c047414ea775f527
Xiphereal <lozanojalex@gmail.com>
2023-06-20 20:20:21 +0200

    refactor(view): replace mouse_entered/exited signals with Area3D.input_event

--- Code/Inputs/Bed.cs
+++ Code/Inputs/Bed.cs
@@ -5,31 +5,18 @@ namespace Godot
     public partial class Bed : Node3D
     {
         private Player player;
-        private bool isMouseOver = false;
 
         public override void _Ready()
         {
             player = new Player();
         }
 
-        public override void _Input(InputEvent @event)
+        public void OnAreaInputEvent(Node camera, InputEvent @event, Vector3 position, Vector3 normal, long shapeIdx)
         {
-            if (@event is InputEventMouseButton mouseButton
-                && mouseButton.Pressed
-                && isMouseOver)
+            if (@event is InputEventMouseButton mouseButton && mouseButton.Pressed)
             {
                 GD.Print(string.Join(", ", player.InteractWith(new Domain.Furniture.Bed())));
             }
-        }
-
-        public void OnMatressMouseEntered()
-        {
-            isMouseOver = true;
-        }
-
-        public void OnMatressMouseExited()
-        {
-            isMouseOver = false;
         }
     }
 }
---
+++ GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage attributes that are applied to
+// this project. Project-level suppressions either have no target or are given a specific target and
+// scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "It's a signal event handler, so they cannot be removed.", Scope = "member", Target = "~M:Godot.Bed.OnAreaInputEvent(Godot.Node,Godot.InputEvent,Godot.Vector3,Godot.Vector3,System.Int64)")]
