a6eceb3a098bcecfb82232c322a1e2caf0148553
Xiphereal <lozanojalex@gmail.com>
2023-12-31 12:55:48 +0100

    feat: item-to-be-bought preview follows (more or less) the mouse cursor

--- Code/Inputs/UI/Buy.cs
+++ Code/Inputs/UI/Buy.cs
@@ -2,10 +2,40 @@ using Godot;
 
 public partial class Buy : Button
 {
+    private Node3D item;
+
     public void BuyItem()
     {
-        PackedScene item = ResourceLoader.Load<PackedScene>("res://Scenes/bed.tscn");
+        item = ResourceLoader
+            .Load<PackedScene>("res://Scenes/bed.tscn")
+            .Instantiate<Node3D>();
 
-        AddChild(item.Instantiate());
+        GetTree().Root.AddChild(item);
+    }
+
+    public override void _Process(double delta)
+    {
+        if (item is not null)
+            MoveWithCursor(item);
+    }
+
+    private void MoveWithCursor(Node3D item)
+    {
+        Vector2 mousePosition = GetGlobalMousePosition();
+        Vector3 targetPosition = Asdfljkadfhs(mousePosition);
+
+        const float ArbitraryHeightThatMatchTheFloors = 0.5f;
+        item.Position = new Vector3(
+            targetPosition.X,
+            ArbitraryHeightThatMatchTheFloors,
+            -targetPosition.Y);
+    }
+
+    private Vector3 Asdfljkadfhs(Vector2 mousePosition)
+    {
+        const int ArbitraryZDepthToBestFitTheMouseMovement = 5;
+        return GetViewport()
+            .GetCamera3D()
+            .ProjectPosition(mousePosition, ArbitraryZDepthToBestFitTheMouseMovement);
     }
 }
